5a501890c3feb36cec7f55ec86e8ec924554f648
Repolho Frito <165079313+xjoaopedrox@users.noreply.github.com>
2026-05-07 23:18:14 -0300

    Add second Pokémon and display battle results

--- desafioPokemonPoo/index.php
+++ desafioPokemonPoo/index.php
@@ -2,5 +2,13 @@
 require_once("Modelo/Pokemon.php");
 
 
-$meuPokemon = new Pokemon("Charmander", "Fogo", 80, 50, 15);
-$meuPokemon->batalhar();
+$pokemon1 = new Pokemon("Charmander", "Fogo", 80, 50, 15);
+$pokemon2 = new Pokemon("Squirtle", "Água", 70, 60, 12);
+
+echo "POKÉMON 1\n";
+$pokemon1->mostrarDados();
+$pokemon1->batalhar();
+
+echo "\nPOKÉMON 2\n";
+$pokemon2->mostrarDados();
+$pokemon2->batalhar();
